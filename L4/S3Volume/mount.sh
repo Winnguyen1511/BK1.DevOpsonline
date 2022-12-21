ea75f219cf4d8f0782822d66a16169345dd5acc9
khoanguyen <khoanguyen1507vn@gmail.com>
2022-12-21 19:03:57 +0700

    update L4

---
+++ L4/S3Volume/mount.sh
@@ -0,0 +1,13 @@
+BUCKET_NAME=mys3volume
+ACCESS_KEY_ID=AKIAZ7LRBIM2HODTSTQT
+SECRET_ACCESS_KEY=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
+mount_point=""$PWD"/"$BUCKET_NAME""
+
+
+
+echo ""$ACCESS_KEY_ID":"$SECRET_ACCESS_KEY"" > ${HOME}/.passwd-s3fs
+chmod 600 ${HOME}/.passwd-s3fs
+
+mkdir -p $mount_point
+
+/usr/local/bin/s3fs "$BUCKET_NAME" "$mount_point" -o passwd_file=${HOME}/.passwd-s3fs  -o dbglevel=info -f -o curldbg
