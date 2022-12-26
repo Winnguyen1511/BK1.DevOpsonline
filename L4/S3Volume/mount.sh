29547221ac0c38e3731a700523c78faeb168ab63
khoanguyen <khoanguyen1507vn@gmail.com>
2022-12-26 21:09:59 +0700

    add sumup  projects

--- L4/S3Volume/mount.sh
+++ L4/S3Volume/mount.sh
@@ -1,13 +1,11 @@
 BUCKET_NAME=mys3volume
-ACCESS_KEY_ID=<ACCESS_KEY_ID>
-SECRET_ACCESS_KEY=<SECRET_ACCESS_KEY>
+ACCESS_KEY_ID=AKIAZ7LRBIM2HODTSTQT
+SECRET_ACCESS_KEY=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
 mount_point=""$PWD"/"$BUCKET_NAME""
-
-
 
 echo ""$ACCESS_KEY_ID":"$SECRET_ACCESS_KEY"" > ${HOME}/.passwd-s3fs
 chmod 600 ${HOME}/.passwd-s3fs
 
 mkdir -p $mount_point
 
-/usr/local/bin/s3fs "$BUCKET_NAME" "$mount_point" -o passwd_file=${HOME}/.passwd-s3fs  -o dbglevel=info -f -o curldbg
+s3fs "$BUCKET_NAME" "$mount_point" -o passwd_file=${HOME}/.passwd-s3fs  -o dbglevel=info -f -o curldbg
